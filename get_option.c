ea6affbdc6b692abf80a0db7b05a3a594248b7e1
noemiepham <huynhtieuquy@gmail.com>
2022-07-08 13:51:56 +0200

    update get option fucntion

--- get_option.c
+++ get_option.c
@@ -1,15 +1,29 @@
-#include <main.h>
-/**
-*get_op_func - function get option
-*@s : number
-*Return: a pointer to the function op_add
-*/
+#include "main.h"
 
-int (*get_op_func(char *s))
+/**
+*get_op_func -checj for valid specifier
+*@s : number
+*Return: a pointer to the function ops
+*/
+int (*get_op_func(char s)) (va_list)
 {
-	op_t ops[] = {
-	{"c", print_char},
-	{"s", print_string},
-	{"%", modulo}
-	{NULL, NULL}
-};
+    op_t ops[] = {
+        {'c', _print_char}
+        {'s', _print_string}
+        {'%', _print_percent}
+        {'d', _print_number}
+        {'i', _print_number}
+        {NULL, NULL}
+    };
+    int i = 0;
+
+	while (ops[i].t;)
+	{
+		if (*fomart == *(p[i].t))
+        {
+            return (ops[i].f)
+        }
+		i++;
+	}
+return (NULL);
+}
