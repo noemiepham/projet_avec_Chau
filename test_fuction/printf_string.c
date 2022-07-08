586254c8c177a2de060be6a04f23c0c4d023b239
noemiepham <huynhtieuquy@gmail.com>
2022-07-08 13:55:39 +0200

    update test string

--- test_fuction/printf_string.c
+++ test_fuction/printf_string.c
@@ -1,50 +1,63 @@
- #include <stdarg.h>
- #include <stdio.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
 char _putchar(char c)
 {
-    return (write(1, &c, 1));
+  return (write(1, &c, 1));
 }
-
 
 int _printf(const char *format, ...)
 {
   va_list list;
-  int count = 0;
+ /*  int count = 0; */
   int index = 0;
   char *array;
 
   va_start(list, format);
-    array = va_arg(list, char *);
-  while (format && format[count] != '\0')
+  array = va_arg(list, char *);
+ /*  while (format && format[count] != '\0')
   {
-      count++;
+    count++;
   }
-  
-  
-  while(format && format[index] != '\0')
+ */
+  if (array == NULL)
+  {
+    array = "(NULL)";
+  }
+
+  while (format && format[index] != '\0')
   {
     if (format[index] == '%' && format[index + 1] == 's')
     {
       for (int j = 0; array[j]; j++)
-        { 
-          _putchar(array[j]);
-        }
+      {
+        _putchar(array[j]);
+      }
+      index++;
     }
     else
     {
-      
-      _putchar(format[index + 1]);
+      _putchar(format[index]);
     }
-      index++;
+    index++;
   }
   va_end(list);
+  return (1);
 }
 
-
-int main(void)
+int main()
 {
 
-    _printf("String:[%s]\n", "I am a string !");
-    return (0);
+  _printf("String:[%s]\n", "I am a ttring !");
+  printf("_________________________________________________\n");
+  printf("String:[%s]\n", "I am a string !");
+  printf("_________________________________________________\n");
+  _printf("String:[%s]\n", NULL);
+  printf("_________________________________________________\n");
+  _printf("String:[%s]\n", NULL);
+ /** _printf("String:[%s][%s]\n", "I am a string !", "hello");
+  printf("_________________________________________________\n");
+  printf("String:[%s][%s]\n", "I am a string !", "hello");
+  */
+      return (0);
 }
