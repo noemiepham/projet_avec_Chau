6ec8f24f2fae57a32ffe15689525bb326382b55a
noemiepham <huynhtieuquy@gmail.com>
2022-07-10 08:59:16 +0200

    update op-function

--- _print_number.c
+++ _print_number.c
@@ -14,32 +14,37 @@ int _print_number(va_list list)
 	int myNumber = va_arg(list, int);
 	int tmp_number = myNumber;
 	int *tab = NULL;
-	int diviseur = myNumber;
+	int diviseur = 0;
 	int reste = 0;
 	int counter_size_number = 0;
 
+	if (tmp_number == 0)
+		_putchar('0'); /* printf condition numero 0 */
 	if (tmp_number < 0)
 	{
 		_putchar('-');
 		myNumber = _abs(tmp_number);
+		size_number = 1; /* count signe '-' */
 	}
+	diviseur = myNumber;
 	do {
 		tmp_number = tmp_number / 10;
-		++size_number;
+		++size_number; /*count size de number for create memory static */
 	} while (tmp_number != 0);
-	tab = malloc(size_number * sizeof(int));
+	tab = malloc(size_number * sizeof(int));  /* array contient de numero */
 
 	while (diviseur > 0)
 	{
 		reste = diviseur % 10;
-		tab[counter_size_number] = reste;
+		tab[counter_size_number] = reste;  /* array number */
 		diviseur = diviseur / 10;
-		counter_size_number++;
+		counter_size_number++; /* count size de number for reverstring */
 	}
 
-	while (counter_size_number > 0)
+	while (counter_size_number > 0) /*reverser array de number */
 	{
 		_putchar(tab[--counter_size_number] + '0');
 	}
+	free(tab);
 	return (size_number);
 }
--- main.c
+++ main.c
@@ -7,12 +7,24 @@
  *
  * Return: 0 on success, error code otherwise
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
 int main(void)
 {
 	int len, len2;
-
-	len = _printf("%");
-	len2 = printf("%");
+	_printf("%d + %d = %d\n", -3, 2, (-3 + 2));
+	len = _printf("%d \n", -2147483647);
+	len2 = _printf("%d \n", INT_MIN);
+	len = _printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	len2 = printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
 	fflush(stdout);
 	if (len != len2)
 	{
